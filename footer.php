53c4ebb5460f7f8662d53aa2ad54376fa7b7925c
kdowd <kevin.dowd@gmail.com>
2023-06-25 12:40:37 +1200

    switched to storefront

--- footer.php
+++ footer.php
@@ -1,7 +1,40 @@
-<footer class="site-footer">
-    <div class="section-inner"></div>
-</footer>
-<?php wp_footer(); ?>
-</body>
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
 
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+	<?php do_action( 'storefront_before_footer' ); ?>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+
+			<?php
+			/**
+			 * Functions hooked in to storefront_footer action
+			 *
+			 * @hooked storefront_footer_widgets - 10
+			 * @hooked storefront_credit         - 20
+			 */
+			do_action( 'storefront_footer' );
+			?>
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+	<?php do_action( 'storefront_after_footer' ); ?>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
 </html>
